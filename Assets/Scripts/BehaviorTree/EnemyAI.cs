de3e3387382bf37a52458750fdc59daa918ee70a
JohnnyGran2002 <144031113+JohnnyGran2002@users.noreply.github.com>
2026-04-21 09:39:37 +0200

    Removed Unity Behavio Tree Editor Graph

--- Assets/Scripts/BehaviorTree/EnemyAI.cs
+++ Assets/Scripts/BehaviorTree/EnemyAI.cs
@@ -1,25 +1,39 @@
 using TreeEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
     public Transform playerTransform;
-    public BehaviorTreeData TreeData;
+
+    public float _attackRange = 2f;
+
+    public float _attackWindup = 1f;
+
+    public float _movementSpeed = 5f;
 
     private BTNode _root;
 
     //Build the behavior tree in Start
     private void Start()
     {
-        // Create context
-        BTContext context = new BTContext
-        {
-            enemy = transform,
-            player = playerTransform
-        };
+        // Build tree manually in code
 
-        // Build tree with context
-        _root = BehavioTreeBuilder.BuildTree(TreeData.RootNode, context);
+        SelectorNode rootSelector = new SelectorNode();
+
+        // Attack behavior
+        SequenceNode attackSequence = new SequenceNode();
+        attackSequence.AddChild(new IsPlayerInRange(transform, playerTransform, _attackRange));
+        attackSequence.AddChild(new TestAttackNode(_attackWindup));
+
+        // Movement fallback
+        TestMoveToPlayerNode move = new TestMoveToPlayerNode(transform, playerTransform, _movementSpeed);
+
+        // Add to root
+        rootSelector.AddChild(attackSequence);
+        rootSelector.AddChild(move);
+
+        _root = rootSelector;
     }
 
     private void Update()
