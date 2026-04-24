b451987c3c4265602c64850772b31011d1b1817b
Pixels033 <115891750+Pixels033@users.noreply.github.com>
2026-04-24 10:59:28 +0200

    påbörjat damage system

---
+++ Assets/Scripts/Albins prototyper/Damageable.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Damageable : MonoBehaviour
+{
+    UnityAction<>
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+/*
+public class TargetBehaviour : MonoBehaviour{
+    public UnityAction<TargetBehaviour, int> onAttack = delegate{};
+
+    public void Attack(int damage){
+        onAttack(this, damage);
+    }
+}
+*/
