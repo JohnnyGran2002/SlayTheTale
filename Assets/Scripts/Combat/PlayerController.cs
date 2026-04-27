bada7dedf06c0d1b766f50c5ba66a33187fffed1
JohnnyGran2002 <144031113+JohnnyGran2002@users.noreply.github.com>
2026-04-27 15:49:26 +0200

    Basic player movement with third person camera

---
+++ Assets/Scripts/Combat/PlayerController.cs
@@ -0,0 +1,46 @@
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform _cameraTransform;
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private bool _shouldFaceMoveDirection = false;
+
+    private CharacterController _controller;
+    private Vector3 _moveInput;
+    private Vector3 _Velocity;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveInput = context.ReadValue<Vector2>();
+        Debug.Log($"Move Input: {_moveInput}");
+    }
+
+    private void Update()
+    {
+        Vector3 foward = _cameraTransform.forward;
+        Vector3 right = _cameraTransform.right;
+
+        foward.y = 0;
+        right.y = 0;
+
+        foward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = foward * _moveInput.y + right * _moveInput.x;
+        _controller.Move(moveDirection * _moveSpeed * Time.deltaTime);
+
+        if (_shouldFaceMoveDirection && moveDirection.sqrMagnitude > 0.001f)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 10f * Time.deltaTime);
+        } 
+    }
+}
