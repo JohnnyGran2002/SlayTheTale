32572c220a421554b6225ab4b8168264e7c7cb2f
teokohlin <a20teoko@student.his.se>
2026-05-04 10:54:04 +0200

    Filip Ljud/Musik

--- Assets/Audio/AudioScripts/MusicManager.cs
+++ Assets/Audio/AudioScripts/MusicManager.cs
@@ -10,11 +10,11 @@ public class MusicManager : MonoBehaviour
 {
     public static MusicManager musicManager;
 
-    public Dictionary<string, SoundEvent> currentMusic;
+    public SoundEvent currentMusic;
 
-    private string _musicString;
+    //private string _musicString;
 
-    private AudioMixer _combatMixer;
+    //private AudioMixer _combatMixer;
     
     //Global parameter that will change based on which turn it is
     public SoundParameterIntensity soundIntensityParameter;
@@ -30,7 +30,6 @@ public class MusicManager : MonoBehaviour
             musicManager = this;
             DontDestroyOnLoad(this);
         }
-        currentMusic = new Dictionary<string, SoundEvent>();
         soundIntensityParameter = new SoundParameterIntensity(1f, UpdateMode.Continuous);
     }
     
@@ -39,26 +38,22 @@ public class MusicManager : MonoBehaviour
         
         
         //Sets the intensity to 1, should be changed if the player should have the first turn
-        //soundIntensityParameter.Intensity = 0;
+        soundIntensityParameter.Intensity = 1;
     }
 
     public void InvokeAudioSettings(EventSettings.EventInfo msg)
     {
-        _musicString = msg.SoundEvent.ToString();
-        Debug.Log(_musicString);
-        if (!currentMusic.ContainsKey(_musicString))
-            currentMusic[_musicString] = msg.SoundEvent;
         switch (msg._action)
         {
             //Plays selected music
             case EventSettings.Action.Play:
-                currentMusic[_musicString].MusicPlay(msg.stopAllOtherMusic, msg.allowFadeOut);
+                currentMusic = msg.SoundEvent;
+                currentMusic.MusicPlay(msg.stopAllOtherMusic, msg.allowFadeOut);
                 break;
-            
             //Stops selected music
             case EventSettings.Action.Stop:
-                currentMusic[_musicString].MusicStop(msg.allowFadeOut);
-                currentMusic.Remove(_musicString);
+                currentMusic = msg.SoundEvent;
+                currentMusic.MusicStop(msg.allowFadeOut);
                 break;
             case EventSettings.Action.SetIntensity:
                 soundIntensityParameter.Intensity = msg.intensityValue;
@@ -75,6 +70,6 @@ public class MusicManager : MonoBehaviour
     //Stops all music (very necessary comment)
     public void StopAllMusic()
     {
-        //currentMusic.MusicStop(true);
+        currentMusic.MusicStop(true);
     }
 }
