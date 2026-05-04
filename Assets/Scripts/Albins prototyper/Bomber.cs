5352114092de5711f1bb8d757e5346cba621d59b
JohnnyGran2002 <144031113+JohnnyGran2002@users.noreply.github.com>
2026-05-04 21:15:26 +0200

    card attack hitbox now pararrel with ground/plane

--- Assets/Scripts/Albins prototyper/Bomber.cs
+++ Assets/Scripts/Albins prototyper/Bomber.cs
@@ -36,13 +36,13 @@ public class Bomber : MonoBehaviour
         switch (areaType)
         {
             case Attack.AreaType.Line:
-                Instantiate(prefabline, transform.position + cam.forward * spawnOffset, cam.rotation, transform);
+                Instantiate(prefabline, transform.position + Vector3.ProjectOnPlane(cam.forward, Vector3.up).normalized * spawnOffset, Quaternion.LookRotation(Vector3.ProjectOnPlane(cam.forward, Vector3.up)), transform);
                 break;
             case Attack.AreaType.Cone:
-                Instantiate(prefabcone, transform.position + cam.forward * spawnOffset, cam.rotation, transform);
+                Instantiate(prefabcone, transform.position + Vector3.ProjectOnPlane(cam.forward, Vector3.up).normalized * spawnOffset, Quaternion.LookRotation(Vector3.ProjectOnPlane(cam.forward, Vector3.up)), transform);
                 break;
             case Attack.AreaType.Circle:
-                Instantiate(prefabcircle, transform.position + cam.forward * spawnOffset, cam.rotation, transform);
+                Instantiate(prefabcircle, transform.position + Vector3.ProjectOnPlane(cam.forward, Vector3.up).normalized * spawnOffset, Quaternion.LookRotation(Vector3.ProjectOnPlane(cam.forward, Vector3.up)), transform);
                 break;
             default:
                 Debug.LogException(new Exception("how did you even?"), this);
