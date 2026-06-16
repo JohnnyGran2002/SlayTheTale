c737043dae76c18b057472373ee356714263cde5
EnFlipflop <filip.josef.rask@gmail.com>
2026-06-16 02:47:24 +0200

    lots of work that might be useless

--- Assets/Scenes/PersonalScenes/Map Test/MapGenerator.cs
+++ Assets/Scenes/PersonalScenes/Map Test/MapGenerator.cs
@@ -1,15 +1,19 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class MapGenerator : MonoBehaviour
 {
-    public GameObject node;
-
+    public GameObject nodePrefab;
     public int rows,columns;
-
     [Tooltip("The space between the nodes")]public float spaceHorizontal, spaceVertical;
+    [Space(7),SerializeField] private int numberOfStartingRooms;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private List<GameObject> _nodesList = new List<GameObject>();
+    private NodeLogic _nodeLogic;
+    
     void Start()
     {
         if (rows == 0 || columns == 0)
@@ -17,20 +21,50 @@ public class MapGenerator : MonoBehaviour
             Debug.LogWarning("Rows or colums can't be 0");
             return;
         }
-        
-        for (var i = 0; i < rows; i++)
+
+        GenerateGrid();
+        AssignStartingRooms();
+    }
+
+    private void GenerateGrid()
+    {
+        //Generates a grid of nodes
+        for (var i = 0; i < columns; i++)
         {
-            //Instantiate(node, new Vector3(0 + (spaceHorizontal * i), 0, 0), Quaternion.identity);
-            
-            for (var j = 0; j < columns; j++)
+            for (var j = 0; j < rows; j++)
             {
-                Instantiate(node, new Vector3(0 + (spaceHorizontal * i), 0, 0 + (spaceVertical * j)), Quaternion.identity);
+                GameObject node = Instantiate(nodePrefab, new Vector3(0 + (spaceHorizontal * j), 0, 0 + (spaceVertical * i)), Quaternion.identity);
+                _nodesList.Add(node);
             }
         }
     }
-    // Update is called once per frame
-    void Update()
+
+    private void AssignStartingRooms()
     {
+        //Makes a randomized list of possible nodes to choose from. Fun fact: This is apparently called a Fisher-Yates shuffle!
+        List<int> nodePool = new List<int>();
+        for (var i = 0; i < rows; i++)
+        {
+            nodePool.Add(i);
+        }
+        for (var i = nodePool.Count - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+            (nodePool[i], nodePool[randomIndex]) = (nodePool[randomIndex], nodePool[i]);
+        }
         
+        //Assigns nodes
+        for (var i = 0; i < numberOfStartingRooms; i++)
+        {
+            AssignNode(nodePool[i]);
+            Debug.Log("Generated " + nodePool[i]);
+        }
+    }
+    
+    private void AssignNode(int targetNode)
+    {
+        //Sets the bool "assigned" on target node to true
+        _nodeLogic = _nodesList[targetNode].GetComponent<NodeLogic>();
+        _nodeLogic.assigned = true;
     }
 }
---
+++ Assets/Scenes/PersonalScenes/Map Test/NodeLogic.cs
@@ -0,0 +1,7 @@
+using System;
+using UnityEngine;
+
+public class NodeLogic : MonoBehaviour
+{
+    public bool assigned;
+}
