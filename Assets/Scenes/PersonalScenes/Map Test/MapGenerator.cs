0f1217f1434029d67363e0933a2afb345ad32699
EnFlipflop <filip.josef.rask@gmail.com>
2026-06-17 14:51:45 +0200

    map generation done

--- Assets/Scenes/PersonalScenes/Map Test/MapGenerator.cs
+++ Assets/Scenes/PersonalScenes/Map Test/MapGenerator.cs
@@ -6,14 +6,19 @@ using Random = UnityEngine.Random;
 
 public class MapGenerator : MonoBehaviour
 {
-    public GameObject nodePrefab;
-    public int rows,columns;
+    //To generating the grid of nodes
+    [Header("Dependencies"),SerializeField] private GameObject nodePrefab;
+    [SerializeField] private GameObject bossPrefab;
+    [Space(7), SerializeField] private int rows;
+    [SerializeField] private int columns;
     [Tooltip("The space between the nodes")]public float spaceHorizontal, spaceVertical;
-    [Space(7),SerializeField] private int numberOfStartingRooms;
+    [SerializeField, Space(7)] private int numberOfStartingRooms;
     
-    private List<GameObject> _nodeList = new List<GameObject>();
+    //To generate paths
     private GameObject[,] _grid;
     private NodeLogic _nodeLogic;
+    private GameObject _bossNode;
+    private NodeLogic _bossLogic;
     
     void Start()
     {
@@ -38,12 +43,19 @@ public class MapGenerator : MonoBehaviour
             {
                 GameObject node = Instantiate(nodePrefab, new Vector3(0 + (spaceHorizontal * j), 0, 0 + (spaceVertical * i)), Quaternion.identity);
                 
-                //_nodeLogic = node.GetComponent<NodeLogic>();
+                //Changes the name of nodes for easier debugging, can be removed later
                 node.name = $"Node_{j}_{i}";
 
                 _grid[j, i] = node;
             }
         }
+        //Generating boss node
+        float middleRow = (rows - 1) / 2f;
+        GameObject bossNode = Instantiate(nodePrefab, new Vector3(0 + (spaceHorizontal * middleRow), 0, 0 + (spaceVertical * columns + 1)), Quaternion.identity);
+        bossNode.name = "Boss_Node";
+        _bossLogic = bossNode.GetComponent<NodeLogic>();
+        _bossLogic.assigned = true;
+        _bossNode = bossNode;
     }
 
     private void AssignStartingRooms()
@@ -60,6 +72,7 @@ public class MapGenerator : MonoBehaviour
             (nodePool[i], nodePool[randomIndex]) = (nodePool[randomIndex], nodePool[i]);
         }
         
+        //Sorts the nodes
         nodePool.Sort();
         
         //Assigns nodes
@@ -70,40 +83,104 @@ public class MapGenerator : MonoBehaviour
             Debug.Log(nodePool[i] + " is a starting node");
             AssignNodes(nodePool[i]);
         }
+
+        ConnectEndNodes();
+        PruneUnassignedNodes();
     }
     
     private void AssignNodes(int startingRow)
     {
-        int currentRow = startingRow;
+        var currentRow = startingRow;
     
-        for (int c = 0; c < columns - 1; c++)
+        //Runs a "walker" through every node and checks if it's connected to a previous node or is a starting node
+        //If it is, the walker randomly chooses one of three nodes on the column above
+        for (var c = 0; c < columns - 1; c++)
         {
             GameObject previousNode = _grid[currentRow, c];
             
-            NodeLogic prevLogic = previousNode.GetComponent<NodeLogic>();
+            NodeLogic previousLogic = previousNode.GetComponent<NodeLogic>();
 
-            if (prevLogic.assigned)
+            //Makes sure the node is part of a path
+            if (!previousLogic.assigned) continue;
+            
+            //Randomly chooses left (-1), straight (0) or right (1)
+            var rowShift = Random.Range(-1, 2);
+            var nextRow = currentRow + rowShift;
+            
+            //Clamps the edges of our grid
+            if (nextRow < 0) nextRow = 0;
+            else if (nextRow >= rows) nextRow = rows - 1;
+
+            //Prevents crossings of paths
+            if (nextRow != currentRow)
             {
-                int rowShift = Random.Range(-1, 2);
-                int nextRow = currentRow + rowShift;
-                    
-                if (nextRow < 0) nextRow = 0;
-                else if (nextRow >= rows) nextRow = rows - 1;
-                        
-                GameObject nextNode = _grid[nextRow, c + 1];
-                
-                NodeLogic nextLogic = nextNode.GetComponent<NodeLogic>();
-                
-                Debug.Log("I'm " + previousNode.name + " and I want to path to " + nextNode.name);
+                GameObject diagonalNode = _grid[nextRow, c];
+                NodeLogic diagonalLogic = diagonalNode.GetComponent<NodeLogic>();
 
-                if (!prevLogic.nextNode.Contains(nextNode))
+                GameObject targetOfDiagonal = _grid[currentRow, c + 1];
+
+                if (diagonalLogic.nextNode.Contains(targetOfDiagonal))
                 {
-                    prevLogic.nextNode.Add(nextNode);
+                    nextRow = currentRow;
                 }
+
+            }
+            
+            //Makes the path connection
+            GameObject nextNode = _grid[nextRow, c + 1];
                 
-                nextLogic.assigned = true;
+            NodeLogic nextLogic = nextNode.GetComponent<NodeLogic>();
+                
+            Debug.Log("I'm " + previousNode.name + " and I want to path to path to " + nextNode.name);
+
+            if (!previousLogic.nextNode.Contains(nextNode))
+            {
+                previousLogic.nextNode.Add(nextNode);
+            }
+                
+            nextLogic.assigned = true;
                         
-                currentRow = nextRow;
+            //Resets the loop
+            currentRow = nextRow;
+        }
+    }
+
+    private void ConnectEndNodes()
+    {
+        //Connects the last nodes to the boss node
+        var endColumnId = columns - 1;
+        
+        for (var i = 0; i < rows; i++)
+        {
+            GameObject endNode = _grid[i, endColumnId];
+            NodeLogic endLogic = endNode.GetComponent<NodeLogic>();
+
+            if (!endLogic.assigned) continue;
+
+            if (!endLogic.nextNode.Contains(_bossNode))
+            {
+                endLogic.nextNode.Add(_bossNode);
+            }
+        }
+    }
+    
+    private void PruneUnassignedNodes()
+    {
+        //Deletes all unassigned nodes, both from the scene and from the array
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < columns; j++)
+            {
+                GameObject currentNode = _grid[i, j];
+                
+                if (currentNode == null) continue;
+
+                NodeLogic nodeLogic = currentNode.GetComponent<NodeLogic>();
+
+                if (nodeLogic.assigned) continue;
+                
+                Destroy(currentNode);
+                _grid[i, j] = null;
             }
         }
     }
