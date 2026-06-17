08fb1ed48f74b843810ab3e25a4de47e6d6fbaf7
EnFlipflop <filip.josef.rask@gmail.com>
2026-06-17 03:58:38 +0200

    node are making paths
    my sleep schedule is fucked :3

--- Assets/Scenes/PersonalScenes/Map Test/MapGenerator.cs
+++ Assets/Scenes/PersonalScenes/Map Test/MapGenerator.cs
@@ -11,7 +11,8 @@ public class MapGenerator : MonoBehaviour
     [Tooltip("The space between the nodes")]public float spaceHorizontal, spaceVertical;
     [Space(7),SerializeField] private int numberOfStartingRooms;
     
-    private List<GameObject> _nodesList = new List<GameObject>();
+    private List<GameObject> _nodeList = new List<GameObject>();
+    private GameObject[,] _grid;
     private NodeLogic _nodeLogic;
     
     void Start()
@@ -25,16 +26,22 @@ public class MapGenerator : MonoBehaviour
         GenerateGrid();
         AssignStartingRooms();
     }
-
+    
     private void GenerateGrid()
     {
+        _grid = new GameObject[rows, columns];
+        
         //Generates a grid of nodes
         for (var i = 0; i < columns; i++)
         {
             for (var j = 0; j < rows; j++)
             {
                 GameObject node = Instantiate(nodePrefab, new Vector3(0 + (spaceHorizontal * j), 0, 0 + (spaceVertical * i)), Quaternion.identity);
-                _nodesList.Add(node);
+                
+                //_nodeLogic = node.GetComponent<NodeLogic>();
+                node.name = $"Node_{j}_{i}";
+
+                _grid[j, i] = node;
             }
         }
     }
@@ -53,18 +60,51 @@ public class MapGenerator : MonoBehaviour
             (nodePool[i], nodePool[randomIndex]) = (nodePool[randomIndex], nodePool[i]);
         }
         
+        nodePool.Sort();
+        
         //Assigns nodes
         for (var i = 0; i < numberOfStartingRooms; i++)
         {
-            AssignNode(nodePool[i]);
-            Debug.Log("Generated " + nodePool[i]);
+            _nodeLogic = _grid[i, 0].GetComponent<NodeLogic>();
+            _nodeLogic.assigned = true;
+            Debug.Log(nodePool[i] + " is a starting node");
+            AssignNodes(nodePool[i]);
         }
     }
     
-    private void AssignNode(int targetNode)
+    private void AssignNodes(int startingRow)
     {
-        //Sets the bool "assigned" on target node to true
-        _nodeLogic = _nodesList[targetNode].GetComponent<NodeLogic>();
-        _nodeLogic.assigned = true;
+        int currentRow = startingRow;
+    
+        for (int c = 0; c < columns - 1; c++)
+        {
+            GameObject previousNode = _grid[currentRow, c];
+            
+            NodeLogic prevLogic = previousNode.GetComponent<NodeLogic>();
+
+            if (prevLogic.assigned)
+            {
+                int rowShift = Random.Range(-1, 2);
+                int nextRow = currentRow + rowShift;
+                    
+                if (nextRow < 0) nextRow = 0;
+                else if (nextRow >= rows) nextRow = rows - 1;
+                        
+                GameObject nextNode = _grid[nextRow, c + 1];
+                
+                NodeLogic nextLogic = nextNode.GetComponent<NodeLogic>();
+                
+                Debug.Log("I'm " + previousNode.name + " and I want to path to " + nextNode.name);
+
+                if (!prevLogic.nextNode.Contains(nextNode))
+                {
+                    prevLogic.nextNode.Add(nextNode);
+                }
+                
+                nextLogic.assigned = true;
+                        
+                currentRow = nextRow;
+            }
+        }
     }
 }
--- Assets/Scenes/PersonalScenes/Map Test/NodeLogic.cs
+++ Assets/Scenes/PersonalScenes/Map Test/NodeLogic.cs
@@ -1,7 +1,24 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NodeLogic : MonoBehaviour
 {
     public bool assigned;
+    public List<GameObject> nextNode = new List<GameObject>();
+    
+    private void OnDrawGizmos()
+    {
+        if (nextNode == null) return;
+
+        foreach (var node in nextNode)
+        {
+            if (node == null) continue;
+
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(transform.position, node.transform.position);
+            Gizmos.DrawSphere(transform.position, 0.15f);
+        }
+    }
+    
 }
