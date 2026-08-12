699d2374c3a098f56463446cbf03b251f3771598
JohnnyGran2002 <144031113+JohnnyGran2002@users.noreply.github.com>
2026-08-12 12:24:50 +0200

    Klar med simpel version av minion spawning

---
+++ Assets/Scripts/Combat/SpawnMinion.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMinion : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> notSpawned = new List<GameObject>();
+    private List<GameObject> alreadySpawned = new List<GameObject>();
+    [SerializeField] private int spawnAmount = 0;
+    [SerializeField] GameObject explosiveMosquito;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SpawnMinionAction(GameObject gameObject)
+    {
+        for (int i = 0; i < spawnAmount; i++)
+        {
+            if (notSpawned.Count > 0)
+            {
+                int randomEnemy = Random.Range(0, notSpawned.Count);
+                Instantiate(notSpawned[randomEnemy]);
+                alreadySpawned.Add(notSpawned[randomEnemy]);
+                notSpawned.RemoveAt(randomEnemy);
+            }
+            else
+            {
+                Instantiate(explosiveMosquito);
+            }
+        }
+    }
+}
