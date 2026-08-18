c69340f1b9094b00f72a2622677523c8650dfbc1
EnFlipflop <filip.josef.rask@gmail.com>
2026-08-18 07:56:11 +0200

    Update RewardCanvas.cs

--- Assets/Scripts/Rewards/RewardCanvas.cs
+++ Assets/Scripts/Rewards/RewardCanvas.cs
@@ -7,25 +7,6 @@ public class RewardCanvas : MonoBehaviour
 
     [SerializeField] private Transform target;
     [SerializeField] private GameObject rewardPrefab;
-
-    /*
-    public void Awake()
-    {
-        gameObject.SetActive(false);
-    }
-
-     public void GiveReward()
-    {
-        for (var i = 0; i < _data.choices.Length; i++)
-        {
-            var currentChoice = Instantiate(choicePrefab, new Vector3(choiceTarget.transform.position.x, choiceTarget.transform.position.y + (i * -spaceBetweenChoices), choiceTarget.transform.position.z), Quaternion.identity, this.transform);
-            currentChoice.SetActive(true);
-            var currentLogic = currentChoice.GetComponent<ChoiceLogic>();
-            Debug.Log(_data.choices[i].Text);
-            currentLogic.text.text = _data.choices[i].Text;
-            currentLogic.choices = _data.choices[i];
-            _choiceList.Add(currentChoice);
-        }
-    }
+    
 } 
-*/
+
