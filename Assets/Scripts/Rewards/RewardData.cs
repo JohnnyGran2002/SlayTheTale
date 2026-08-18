f8238505eb4cab9cc75f5b5605228c751db378a9
EnFlipflop <filip.josef.rask@gmail.com>
2026-08-18 19:59:22 +0200

    end of the day
    TODO: Finish reward

--- Assets/Scripts/Rewards/RewardData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Reward", menuName = "RewardData", order = 1)]
-
-public class RewardData : ScriptableObject
-{
-    [Tooltip("Cards that will be given to the player")]public CardData[] card;
-    public int money, maxHp;
-}
